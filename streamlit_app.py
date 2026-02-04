f5b2ce2740acb4539a31d5043b3c464509f1920b
felixafernandezportugues-ux <felixafernandezportugues@gmail.com>
2026-02-04 09:36:21 +0100

    Update streamlit_app.py
    fix to search the correct page

--- streamlit_app.py
+++ streamlit_app.py
@@ -68,7 +68,7 @@ if ingredients_list:
         st.write('The search alue for ', fruit_chosen,' is ', search_on, '.')
 
         st.subheader(fruit_chosen + ' Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+ fruit_chosen)
+        smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}")
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     st.write(f'La lista de ingredientes es: {ingredients_string}')
 
